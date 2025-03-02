0f5db4dbdc39c5cee1c7699bc52e637e0a4226a5
Ivan Muzyka <musicvano@gmail.com>
2025-03-02 21:23:05 +0200

    Update home page

---
+++ Rux.Web/Controllers/BlogController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rux.Web.Controllers;
+
+public class BlogController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Rux.Web/Controllers/DocsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rux.Web.Controllers;
+
+public class DocsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
